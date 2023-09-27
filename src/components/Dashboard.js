78600338c8c6cd21c400fa45bd183e7d1860c3de
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-26 22:17:20 -0400

    create the panel, pass props to panel, add mock data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,56 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10,
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4,
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng",
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza",
+  },
+];
 
 class Dashboard extends Component {
   state = {
-    loading: true
+    loading: true,
   };
 
   render() {
     const dashboardClasses = classnames("dashboard");
+    const panels = data.map((panel) => {
+      return (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      );
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    return <main className={dashboardClasses}>
+      {panels}
+      </main>;
   }
 }
 
