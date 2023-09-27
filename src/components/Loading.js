adc3a2ef8b73e12876757b7c0044988f26ae56e8
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-26 20:38:43 -0400

    Add Loading Component

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classnames from "classnames";
+
+ class Loading extends Component {
+ render() {
+  return <section className="loading">Loading</section>;
+ }
+}
+
+export default Loading;
